7193f10aa08815c3d496f820ca7facbcadc62eda
Christian Schneemann <schneemann@b1-systems.de>
2012-04-05 13:00:48 +0200

    Revert "fixed overwriting $eok by do_subst"
    This reverts commit b4d00389e4ebc9f66d1abeffcf6057875809849c.

--- Build.pm
+++ Build.pm
@@ -485,7 +485,7 @@ sub get_deps {
 my $config_bak = $config;
   ($eok, @deps) = expand($config, @deps, @ndeps);
   if($eok) {
-    @deps = do_subst($config_bak, @deps); 
+    ($eok, @deps) = do_subst($config_bak, @deps); 
     %crossdeps = (%crossdeps, extract_crossdeps(@deps));
     @deps = drop_crossdeps(@deps);
   }
